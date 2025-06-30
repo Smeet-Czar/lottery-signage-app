7e46a9a64ad195685c567d050d82f12e23b3968b
Smeet-Czar <smeetrp2@illinois.edu>
2025-06-29 20:06:25 -0400

    scraper

---
+++ app/scraper.py
@@ -0,0 +1,9 @@
+import bs4
+import requests
+from selenium import webdriver
+import os
+import time
+
+chromeDriverPath = r'C:\Users\smeet\OneDrive\Desktop\Project_Lottery_DLS\chromedriver-win64\chromedriver.exe'
+driver = webdriver.Chrome(chromeDriverPath)
+
