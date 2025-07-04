68219060ece0220716cba55e97e0df6ad1d8b8ba
Smeet-Czar <smeetrp2@illinois.edu>
2025-07-04 12:59:57 -0400

    scraped data

--- app/scraper.py
+++ app/scraper.py
@@ -2,6 +2,7 @@ import bs4
 import requests
 import os
 import time
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -26,9 +27,6 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://www.flalottery.com/scratch-offs")
 time.sleep(2)  # Wait for page to fully load
 
-# <span class="cmp-button__text">View More</span>
-# //*[@id="scratchoffsearch-6c58e32c1d"]/div[2]/button
-# <button class="cmp-button spacing-t-none spacing-b-none min-w-[12rem] justify-center bg-beige-200 text-black" type="button"><!----><span class="cmp-button__text">View More</span></button>
 while True:
     try:
         view_more = driver.find_element(By.XPATH, "//button[.//span[text()='View More']]")  
@@ -39,14 +37,73 @@ while True:
     except:
         break
     
-
+tickets_data = []
 # (Get names of lottery present in current page)
 elements = driver.find_elements(By.TAG_NAME, "li")
 for el in elements:
     try:
         name = el.find_element(By.CLASS_NAME, "inline-block.focus-pink").text
-        print(name)
+        top_prize = el.find_element(By.CLASS_NAME, "text-lg").text
+        ticket_price = el.find_element(By.XPATH, ".//dt[text()='Price']/following-sibling::dd[1]").text
+        ticket_info = {
+            "Name": name,
+            "Top Prize": top_prize,
+            "Ticket Price": ticket_price
+        }
+        tickets_data.append(ticket_info)
+
     except:
         continue
+df = pd.DataFrame(tickets_data)
+
+# Optional: Save to CSV
+df.to_csv("lottery_tickets_data.csv", index=False)
+
+# Print preview
+print(df.head())
+# 🎯 Find all scratch-off ticket cards
 
 
+# <dd class="text-lg">$5,000,000</dd>   Price money
+# <dt class="sr-only">Price</dt> Price of ticket
+# <span class="block bg-center bg-cover rounded-t-lg bg-beige-300 ratio ratio-4:3 lazyloaded" data-bg="/content/dam/flalottery-web/images/games/scratch-offs/$5,000,000-cash-money/fl-1594-$5000000-cash-money-teaser@2x.png/jcr:content/renditions/cq5dam.web.1280.1280.jpeg" style="background-image: url(&quot;/content/dam/flalottery-web/images/games/scratch-offs/$5,000,000-cash-money/fl-1594-$5000000-cash-money-teaser@2x.png/jcr:content/renditions/cq5dam.web.1280.1280.jpeg&quot;);"></span>
+# //*[@id="scratchoffsearch-6c58e32c1d-vc-results"]/li[1]/article/div/div/dl/div[2]/dd
+
+
+# elements = driver.find_elements(By.TAG_NAME, "li")
+# elements = driver.find_elements(By.CSS_SELECTOR, "li.game-item")
+
+# for element in elements:
+#     name = element.find_element(By.CSS_SELECTOR, ".inline-block.focus-pink").text
+#     top_prize = element.find_element(By.CLASS_NAME, "text-lg").text
+#     ticket_price = element.find_element(By.XPATH, ".//dt[text()='Price']/following-sibling::dd[1]").text
+#     tickets_data = []
+
+#     print(top_prize)
+
+# for el in elements:
+#     try:
+#         name = el.find_element(By.CSS_SELECTOR, ".inline-block.focus-pink").text
+#         top_prize = el.find_element(By.CLASS_NAME, "text-lg").text
+#         ticket_price = el.find_element(By.XPATH, ".//dt[text()='Price']/following-sibling::dd[1]").text
+
+#         ticket_info = {
+#             "Name": name,
+#             "Top Prize": top_prize,
+#             "Ticket Price": ticket_price
+#         }
+#         tickets_data.append(ticket_info)
+#         print(ticket_info)
+
+#     except Exception as e:
+#         print(f"❌ Error: {e}")
+#         continue
+
+#print(tickets_data)
+# df = pd.DataFrame(tickets_data)
+
+# # Optional: Save to CSV
+# df.to_csv("lottery_tickets_data.csv", index=False)
+
+# # Print preview
+# print(df.head())
